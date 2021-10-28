152d1b9dc422f8bfdd7880d954822f4e8b2a8ecd
meen0natnisha <meen0natnisha@gmail.com>
2021-10-28 16:51:50 +0700

    update graph

--- functions/show_graph.py
+++ functions/show_graph.py
@@ -1,7 +1,29 @@
+import matplotlib.pyplot as plt
+from functions.df_tools import *
+import pandas as pd
+
+def show_graph(seleted_graph):
+    if seleted_graph == 1:
+        f = readTxt('user.txt')
+        print("Amount of people of Registrants in the same age.")
+        data = f.groupby(['age']).size().reset_index(name='counts')
+        df = pd.DataFrame(data ,columns=['age','counts'])
+        df.plot(x ='age', y='counts', kind = 'bar')
+        plt.show()
+    elif seleted_graph == 2: 
+        f = readTxt('user.txt')
+        print("Amount of people of Reschedule in the same day.")
+        data = f.groupby(['age']).size().reset_index(name='counts')
+        df = pd.DataFrame(data ,columns=['age','counts'])
+        df.plot(x ='age', y='counts', kind = 'bar')
+        plt.show()
+
+
 def main():
     print("Show graph")
     seleted_graph = int(input("Select graph 1-10\n: "))
-    pass # show graph
+    show_graph(seleted_graph)
+    # print(graph)
 
 if __name__ == '__main__':
     main()
