764fb1e5a4e225fbc6a5050c556cc8f84dc44d39
ZShiroeSamaZ <47985474+ZShiroeSamaZ@users.noreply.github.com>
2021-10-28 23:42:38 +0700

    merge graph

--- functions/show_graph.py
+++ functions/show_graph.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
 import matplotlib.pyplot as plt
 from functions.df_tools import *
 import pandas as pd
+import numpy as np
 
 def bar_plot(ax, data, group_stretch=0.8, bar_stretch=0.95,
              legend=True, x_labels=True, label_fontsize=8,
@@ -44,6 +46,10 @@ def bar_plot(ax, data, group_stretch=0.8, bar_stretch=0.95,
         ax.set_xticklabels()
     return bars, group_centers
 
+def filter_df(df, key_column, key_value):
+    df = df.loc[(df[key_column] == key_value)]
+    return df
+    
 def show_graph(seleted_graph):
     if seleted_graph == 1:
         f = readTxt('user.txt')
@@ -84,10 +90,78 @@ def show_graph(seleted_graph):
         # vaccine.pivot(index='age', columns='first dose', values='counts').plot(kind='bar')
         # plt.title("Amount of people of Registrants with vaccine.")
         # plt.show()
+    elif seleted_graph == 6:
+        f = readTxt('schedual.txt')
+        print("Location with Amount of people of Registrants.")
+        data = f.groupby(['location']).size().reset_index(name='counts')
+        df = pd.DataFrame(data ,columns=['location','counts'])
+        ax = df.plot(x ='location', y='counts', kind = 'bar')
+        ax.set_xlabel("Location")
+        ax.set_ylabel("Amount of people")
+        for container in ax.containers:
+            ax.bar_label(container)
+        plt.show()
+    elif seleted_graph == 7: 
+        f = readTxt('schedual.txt')
+        print("Amount of vaccine of Registrants on the same day.")
+        data = f.filter(items=['Vaccine name', 'date']).groupby(['date', 'Vaccine name']).size().reset_index(name='counts')
+        ax = data.pivot(index='date', columns='Vaccine name', values='counts').plot(kind='bar')
+        ax.set_ylabel("Amount of vaccine of Registrants (Person )")
+        for container in ax.containers:
+            ax.bar_label(container)
+        plt.show()
+    elif seleted_graph == 8: 
+        f = readTxt("vaccine_amount.txt")
+        print("Remaining of vaccine.")
+        values = f.values[-1]
+        fig, ax = plt.subplots()
+        data = {"Sinovac": [values[0]], "Sinopharm": [values[1]], "AstraZeneca": [values[2]], "Pfizer": [values[3]]}
+        bar_plot(ax, data, group_stretch=0.8, bar_stretch=0.95, legend=True,
+                x_labels=True, label_fontsize=8, barlabel_offset=0.05,
+                bar_labeler=lambda k, i, s: str(round(s, 3)))
+        ax.set_xlabel("Vaccine name")
+        ax.set_ylabel("Amount of vaccine (Dose)")
+        fig.show()
+
+        plt.show() 
+    elif seleted_graph == 9:
+        f = readTxt("situation_status.txt")
+        print("Number of round of check the registration situation on the same day.")
+        registration = f.filter(items=['registration', 'date']).groupby(['date']).aggregate("sum")
+        df = pd.DataFrame(registration ,columns=['registration'])
+        df = df.drop(df[df.registration < 1].index)
+        ax = df.plot(y='registration', kind = 'bar', use_index = True)
+        ax.set_xlabel("Date")
+        ax.set_ylabel("Number of round")
+        for container in ax.containers:
+            ax.bar_label(container)
+        plt.show() 
+    elif seleted_graph == 10:
+        f = readTxt("situation_status.txt")
+        print("Number of round of Check vaccination historyon the same day.")
+        history = f.filter(items=['history', 'date']).groupby(['date']).aggregate("sum")
+        df = pd.DataFrame(history ,columns=['history'])
+        df = df.drop(df[df.history < 1].index)
+        ax = df.plot(y='history', kind = 'bar', use_index = True)
+        ax.set_xlabel("Date")
+        ax.set_ylabel("Number of round")
+        for container in ax.containers:
+            ax.bar_label(container)
+        plt.show() 
 
 
 def main():
     print("Show graph")
+    print("1. Amount of people of Registrants on the same day.")
+    print("2. Amount of people of Reschedule on the same day.")
+    print("3. Amount of people of Bedridden patient and not a bedridden patient on the same day.")
+    print("4. Amount of people of Bedridden patient by age.")
+    print("5. Amount of people of Registrants with Vaccine.")
+    print("6. Location with Amount of people of Registrants.")
+    print("7. Amount of vaccine of Registrants on the same day.")
+    print("8. Remaining of vaccine.")
+    print("9. Number of round of check the registration situation on the same day.")
+    print("10. Number of round of Check vaccination history on the same day. ")
     seleted_graph = int(input("Select graph 1-10\n: "))
     show_graph(seleted_graph)
     # print(graph)
